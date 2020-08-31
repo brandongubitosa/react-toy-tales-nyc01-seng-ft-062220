7f0df91f633400a24f7abfaa8832ed0b68e388d7
scubaguba345 <brandon.gubitosa@gmail.com>
2020-08-31 10:00:59 -0400

    Done.

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -4,31 +4,38 @@ class ToyCard extends Component {
 
   state = {
     likes: this.props.toy.likes
+   
   }
 
   increaseLikes = () => {
-    
     const newLikes = this.state.likes + 1
-    console.log(newLikes)
-    this.setState({
-      likes: newLikes
+    //console.log(newLikes)
+    this.setState({likes: newLikes})
+
+    fetch(`http://localhost:3000/toys/${this.props.toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+        "accepts": "application/json"
+      },
+      body: JSON.stringify(this.state)
     })
-    //i need to make a patch request in here to update the number of likes to the backend
-    //then in toyContainer do another Component Did Update
+    
   }
   
-
 
 
   render() {
   
+    let id = this.props.toy.id
     return (
-      <div id={this.props.toy.id} className="card">
+    
+      <div className="card">
         <h2>{this.props.toy.name}</h2>
         <img src={this.props.toy.image} alt={this.props.toy.name} className="toy-avatar" />
         <p>{this.props.toy.likes} Likes </p>
         <button  className="like-btn"onClick={this.increaseLikes}>Like {'<3'}</button>
-        <button  className="del-btn" onClick={this.deleteHandler}>Donate to GoodWill</button>
+        <button className="del-btn" onClick={()=> {id !== undefined ? this.props.deleteHandler(id) : console.log("CANT")} }>Donate to GoodWill</button>
       </div>
     );
   }
